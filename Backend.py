1ae8dcbf67fcbb1e47494f21cd8287d56936b631
Allen <wildman279@gmail.com>
2015-08-09 13:00:02 -0400

    converted tabs to spaces in Main and Backend files; no other changes

--- Backend.py
+++ Backend.py
@@ -5,144 +5,144 @@ import pdb
 
 class Backend(object):
 
-	def __init__(self, arg=None):
-		if arg is None:
-			self._default_config()
-		elif type(arg) == dict:
-			self._import_config_dict(arg)
-		elif type(arg) == str:
-			# if arg is a string, assume it's a json filename
-			try:
-				with open(arg) as fp:
-					js = json.load(fp)
-					self._import_config_dict(js)
-			except:
-				print("Error: Unable to load %s! Using default config"%arg)
-				self._default_config()
-		else:
-			print("Error: Unknown constructor argument type! Using default config")
-			self._default_config()
+    def __init__(self, arg=None):
+        if arg is None:
+            self._default_config()
+        elif type(arg) == dict:
+            self._import_config_dict(arg)
+        elif type(arg) == str:
+            # if arg is a string, assume it's a json filename
+            try:
+                with open(arg) as fp:
+                    js = json.load(fp)
+                    self._import_config_dict(js)
+            except:
+                print("Error: Unable to load %s! Using default config"%arg)
+                self._default_config()
+        else:
+            print("Error: Unknown constructor argument type! Using default config")
+            self._default_config()
 
-	def _default_config(self):
-		self._recipes = {}
-		self._nic_base = 'vg'
-		self._nic_strength = 100.0
+    def _default_config(self):
+        self._recipes = {}
+        self._nic_base = 'vg'
+        self._nic_strength = 100.0
 
 
-	def _import_config_dict(self, arg):
-		if '_recipes' in arg.keys():
-			self._recipes = self._check_recipes(arg['_recipes'])
-			try:
-				self._nic_base = arg['_config']['nic_base']
-			except KeyError:
-				self._nic_base = 'vg'
-			try:
-				self._nic_strength = arg['_config']['nic_strength']
-			except KeyError:
-				self._nic_strength = 100
-		else:
-			# if there's no _recipes dict inside, then assume the whole file is
-			# a dict of recipes, and use defaults for nic stuff
-			self._default_config()
-			self._recipes = self._check_recipes(arg)
+    def _import_config_dict(self, arg):
+        if '_recipes' in arg.keys():
+            self._recipes = self._check_recipes(arg['_recipes'])
+            try:
+                self._nic_base = arg['_config']['nic_base']
+            except KeyError:
+                self._nic_base = 'vg'
+            try:
+                self._nic_strength = arg['_config']['nic_strength']
+            except KeyError:
+                self._nic_strength = 100
+        else:
+            # if there's no _recipes dict inside, then assume the whole file is
+            # a dict of recipes, and use defaults for nic stuff
+            self._default_config()
+            self._recipes = self._check_recipes(arg)
 
-	def _check_recipes(self, recipes):
-		""" parse the recipes dict given to make sure they're valid
-		and automatically remove invalid recipes
-		TODO: actually implement this
-		TODO: incorporate Qt signals for GUI warning messages"""
+    def _check_recipes(self, recipes):
+        """ parse the recipes dict given to make sure they're valid
+        and automatically remove invalid recipes
+        TODO: actually implement this
+        TODO: incorporate Qt signals for GUI warning messages"""
 
-		ret = {}
-		if type(recipes) is not dict:
-			print("Error: recipes is not type 'dict'!")
-			return ret
+        ret = {}
+        if type(recipes) is not dict:
+            print("Error: recipes is not type 'dict'!")
+            return ret
 
-		for (recipe, flavors) in recipes.items():
-			if type(flavors) is not dict:
-				print("Error: recipe %s does not contain a dict of flavors"%recipe)
-				continue
-			ret[recipe] = {}
-			for (flav, amount) in flavors.items():
-				if type(amount) is not int and type(amount) is not float:
-					print("Error: flavor %s has non-numeric amount: %s"%(flav, amount))
-					continue
-				# always assume percent
-				amount = amount / 100.0
-				ret[recipe][flav] = amount
+        for (recipe, flavors) in recipes.items():
+            if type(flavors) is not dict:
+                print("Error: recipe %s does not contain a dict of flavors"%recipe)
+                continue
+            ret[recipe] = {}
+            for (flav, amount) in flavors.items():
+                if type(amount) is not int and type(amount) is not float:
+                    print("Error: flavor %s has non-numeric amount: %s"%(flav, amount))
+                    continue
+                # always assume percent
+                amount = amount / 100.0
+                ret[recipe][flav] = amount
 
-		return ret
+        return ret
 
-	def calculate_mix(self, recipe_name, totalvol=10, nic=3, vg=70, mix='juice_from_ingredients'):
-		""" Calculate the mix for the given recipe and parameters
-		TODO: check for max PG/VG and correct appropriately
-		"""
-		try:
-			#pdb.set_trace()
-			recipe = self._recipes[recipe_name]
-		except KeyError:
-			print("Error: recipe %s not found!"%recipe_name)
-			return None
+    def calculate_mix(self, recipe_name, totalvol=10, nic=3, vg=70, mix='juice_from_ingredients'):
+        """ Calculate the mix for the given recipe and parameters
+        TODO: check for max PG/VG and correct appropriately
+        """
+        try:
+            #pdb.set_trace()
+            recipe = self._recipes[recipe_name]
+        except KeyError:
+            print("Error: recipe %s not found!"%recipe_name)
+            return None
 
-		ret = {}
-		if vg > 1:
-			vg = vg / 100.0
+        ret = {}
+        if vg > 1:
+            vg = vg / 100.0
 
-		
-		# assumes all flavors are PG, will fix this later
-		totalflav = sum([1.0*totalvol*recipe[f] for f in recipe.keys()])
-		nic = 1.0*nic*totalvol / self._nic_strength
+        
+        # assumes all flavors are PG, will fix this later
+        totalflav = sum([1.0*totalvol*recipe[f] for f in recipe.keys()])
+        nic = 1.0*nic*totalvol / self._nic_strength
 
-		# make sure we're within Max VG/Min VG range
-		max_vg = 1.0 - self.get_total_flavor(recipe_name) - (nic if self._nic_base=='pg' else 0)
-		if vg > max_vg:
-			vg = max_vg
-			ret['message'] = 'Using Max VG: %.1f%%'%(max_vg*100.0)
-		else:
-			min_vg = nic / totalvol if self._nic_base=='vg' else 0.0
-			if vg < min_vg:
-				vg = min_vg
-				ret['message'] = 'Using Max PG: %.1f%%'%(100.0*(1.0-min_vg))
+        # make sure we're within Max VG/Min VG range
+        max_vg = 1.0 - self.get_total_flavor(recipe_name) - (nic if self._nic_base=='pg' else 0)
+        if vg > max_vg:
+            vg = max_vg
+            ret['message'] = 'Using Max VG: %.1f%%'%(max_vg*100.0)
+        else:
+            min_vg = nic / totalvol if self._nic_base=='vg' else 0.0
+            if vg < min_vg:
+                vg = min_vg
+                ret['message'] = 'Using Max PG: %.1f%%'%(100.0*(1.0-min_vg))
 
-		totalvg = totalvol * vg
-		totalpg = totalvol - totalvg
+        totalvg = totalvol * vg
+        totalpg = totalvol - totalvg
 
-		if self._nic_base == 'vg':
-			addpg = totalpg - totalflav
-			addvg = totalvg - nic
-		else:
-			addpg = totalpg - totalflav - nic
-			addvg = totalvg
+        if self._nic_base == 'vg':
+            addpg = totalpg - totalflav
+            addvg = totalvg - nic
+        else:
+            addpg = totalpg - totalflav - nic
+            addvg = totalvg
 
-		# force nonnegative numbers in case rounding makes -.0000000001 or whatever
-		if addpg < 0:
-			addpg = 0
-		if addvg < 0:
-			addvg = 0
+        # force nonnegative numbers in case rounding makes -.0000000001 or whatever
+        if addpg < 0:
+            addpg = 0
+        if addvg < 0:
+            addvg = 0
 
-		ret['pg'] = addpg
-		ret['vg'] = addvg
-		ret['nic'] = nic
-		ret['flavors'] = {}
-		for (f, amount) in recipe.items():
-			ret['flavors'][f] = 1.0*totalvol*amount
+        ret['pg'] = addpg
+        ret['vg'] = addvg
+        ret['nic'] = nic
+        ret['flavors'] = {}
+        for (f, amount) in recipe.items():
+            ret['flavors'][f] = 1.0*totalvol*amount
 
-		return ret
+        return ret
 
-	def get_recipes(self):
-		recs = list(self._recipes.keys())
-		recs.sort()
-		return recs
+    def get_recipes(self):
+        recs = list(self._recipes.keys())
+        recs.sort()
+        return recs
 
-	def get_config(self):
-		return {'n_recipes': len(self._recipes.keys()),
-				'nic_strength': self._nic_strength,
-				'nic_base': self._nic_base}
+    def get_config(self):
+        return {'n_recipes': len(self._recipes.keys()),
+                'nic_strength': self._nic_strength,
+                'nic_base': self._nic_base}
 
-	def get_total_flavor(self, recipe_name):
-		try:
-			recipe = self._recipes[recipe_name]
-		except KeyError:
-			print("Error: recipe %s not found!"%recipe_name)
-			return None
-		return sum(recipe.values())
+    def get_total_flavor(self, recipe_name):
+        try:
+            recipe = self._recipes[recipe_name]
+        except KeyError:
+            print("Error: recipe %s not found!"%recipe_name)
+            return None
+        return sum(recipe.values())
 
--- Main.py
+++ Main.py
@@ -9,158 +9,158 @@ import pdb
 CONFIG_FILE = 'vaperecipes.json'
 
 class YaccMain(QtGui.QMainWindow):
-	def __init__(self, parent=None):
-		self.is_init = False
-		QtGui.QWidget.__init__(self, parent)
-		self.ui = Ui_yacc_main_window()
-		self.ui.setupUi(self)
+    def __init__(self, parent=None):
+        self.is_init = False
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_yacc_main_window()
+        self.ui.setupUi(self)
 
-		# set up backend and load recipes
-		self.config_file = CONFIG_FILE
-		self.load_config()
+        # set up backend and load recipes
+        self.config_file = CONFIG_FILE
+        self.load_config()
 
-		# Fill in UI defaults
-		self.ui.totalvol_box.setText('10')
-		self.ui.nic_box.setText('2')
-		self.ui.vg_box.setText('70')
+        # Fill in UI defaults
+        self.ui.totalvol_box.setText('10')
+        self.ui.nic_box.setText('2')
+        self.ui.vg_box.setText('70')
 
-		# add combo box options
-		self.ui.mix_box.addItem('Juice from Ingredients', 'from_ingredients')
-		self.ui.mix_box.addItem('Juice from Concentrate', 'from_concentrate')
-		self.ui.mix_box.addItem('Concentrate', 'concentrate')
+        # add combo box options
+        self.ui.mix_box.addItem('Juice from Ingredients', 'from_ingredients')
+        self.ui.mix_box.addItem('Juice from Concentrate', 'from_concentrate')
+        self.ui.mix_box.addItem('Concentrate', 'concentrate')
 
-		# status bar
-		self.status_config_message_label = QtGui.QLabel()
-		self.ui.status_bar.addPermanentWidget(self.status_config_message_label)
+        # status bar
+        self.status_config_message_label = QtGui.QLabel()
+        self.ui.status_bar.addPermanentWidget(self.status_config_message_label)
 
-		# signals/slots
-		self.ui.actionExit.triggered.connect(self.exit)
-		self.ui.update_button.clicked.connect(self.update_mix)
-		self.ui.recipe_box.currentIndexChanged.connect(self.update_mix)
-		self.ui.recipe_box.currentIndexChanged.connect(self.update_recipe_status)
-		self.ui.mix_box.currentIndexChanged.connect(self.update_mix)
-		self.ui.totalvol_box.textChanged.connect(self.update_mix)
-		self.ui.nic_box.textChanged.connect(self.update_mix)
-		self.ui.vg_box.textChanged.connect(self.update_mix)
-		self.ui.reload_button.clicked.connect(self.load_config)
+        # signals/slots
+        self.ui.actionExit.triggered.connect(self.exit)
+        self.ui.update_button.clicked.connect(self.update_mix)
+        self.ui.recipe_box.currentIndexChanged.connect(self.update_mix)
+        self.ui.recipe_box.currentIndexChanged.connect(self.update_recipe_status)
+        self.ui.mix_box.currentIndexChanged.connect(self.update_mix)
+        self.ui.totalvol_box.textChanged.connect(self.update_mix)
+        self.ui.nic_box.textChanged.connect(self.update_mix)
+        self.ui.vg_box.textChanged.connect(self.update_mix)
+        self.ui.reload_button.clicked.connect(self.load_config)
 
-		self.is_init = True
-		self.update_mix()
-		self.update_config_status()
-		self.update_recipe_status()
+        self.is_init = True
+        self.update_mix()
+        self.update_config_status()
+        self.update_recipe_status()
 
-	def update_mix(self):
-		if not self.is_init:
-			return
+    def update_mix(self):
+        if not self.is_init:
+            return
 
-		mix_inputs = self.check_inputs()
-		if mix_inputs is None:
-			return
+        mix_inputs = self.check_inputs()
+        if mix_inputs is None:
+            return
 
-		mix = self.be.calculate_mix(*mix_inputs)
-		if mix is None:
-			# calculate_mix returns None if the recipe can't be found, so just bail
-			# This shouldn't really happen since recipe_box is only populated by items that
-			# backend.get_recipes returns
-			return
+        mix = self.be.calculate_mix(*mix_inputs)
+        if mix is None:
+            # calculate_mix returns None if the recipe can't be found, so just bail
+            # This shouldn't really happen since recipe_box is only populated by items that
+            # backend.get_recipes returns
+            return
 
-		max_flavor_length = max([len(f) for f in (list(mix['flavors'].keys()) + ['Nicotine'])])
+        max_flavor_length = max([len(f) for f in (list(mix['flavors'].keys()) + ['Nicotine'])])
 
-		self.ui.output_box.setPlainText('')
-		for f in sorted(mix['flavors'].keys()):
-			spaces = max_flavor_length - len(f)
-			self.ui.output_box.appendPlainText('%s: %3.2f mL'%(' '*spaces + f, mix['flavors'][f]))
-		self.ui.output_box.appendPlainText('') # blank line
-		self.ui.output_box.appendPlainText(' '*(max_flavor_length-8) + 'Nicotine: %3.2f mL'%mix['nic'])
-		self.ui.output_box.appendPlainText(' '*(max_flavor_length-2) + 'VG: %3.2f mL'%mix['vg'])
-		self.ui.output_box.appendPlainText(' '*(max_flavor_length-2) + 'PG: %3.2f mL'%mix['pg'])
-		if 'message' in mix:
-			self.ui.output_box.appendPlainText('')
-			self.ui.output_box.appendPlainText(mix['message'])
+        self.ui.output_box.setPlainText('')
+        for f in sorted(mix['flavors'].keys()):
+            spaces = max_flavor_length - len(f)
+            self.ui.output_box.appendPlainText('%s: %3.2f mL'%(' '*spaces + f, mix['flavors'][f]))
+        self.ui.output_box.appendPlainText('') # blank line
+        self.ui.output_box.appendPlainText(' '*(max_flavor_length-8) + 'Nicotine: %3.2f mL'%mix['nic'])
+        self.ui.output_box.appendPlainText(' '*(max_flavor_length-2) + 'VG: %3.2f mL'%mix['vg'])
+        self.ui.output_box.appendPlainText(' '*(max_flavor_length-2) + 'PG: %3.2f mL'%mix['pg'])
+        if 'message' in mix:
+            self.ui.output_box.appendPlainText('')
+            self.ui.output_box.appendPlainText(mix['message'])
 
-	def update_config_status(self):
-		cfg = self.be.get_config()
-		self.status_config_message_label.setText('Nicotine: %d mg/mL %s; Recipes Loaded: %d'%(
-												cfg['nic_strength'], cfg['nic_base'].upper(), cfg['n_recipes']))
-	
-	def load_config(self):
-		is_init_last = self.is_init
-		self.is_init = False # make sure update_mix doesn't fail when the config is cleared out
-		self.be = Backend(self.config_file)
-		selected_recipe = self.ui.recipe_box.currentText()
+    def update_config_status(self):
+        cfg = self.be.get_config()
+        self.status_config_message_label.setText('Nicotine: %d mg/mL %s; Recipes Loaded: %d'%(
+                                                cfg['nic_strength'], cfg['nic_base'].upper(), cfg['n_recipes']))
+    
+    def load_config(self):
+        is_init_last = self.is_init
+        self.is_init = False # make sure update_mix doesn't fail when the config is cleared out
+        self.be = Backend(self.config_file)
+        selected_recipe = self.ui.recipe_box.currentText()
 
-		self.ui.recipe_box.clear()
-		for recipe in self.be.get_recipes():
-			self.ui.recipe_box.addItem(recipe)
+        self.ui.recipe_box.clear()
+        for recipe in self.be.get_recipes():
+            self.ui.recipe_box.addItem(recipe)
 
-		selected_index = self.ui.recipe_box.findText(selected_recipe)
-		if selected_index != -1:
-			self.ui.recipe_box.setCurrentIndex(selected_index)
+        selected_index = self.ui.recipe_box.findText(selected_recipe)
+        if selected_index != -1:
+            self.ui.recipe_box.setCurrentIndex(selected_index)
 
-		self.is_init = is_init_last
-		self.update_mix()
+        self.is_init = is_init_last
+        self.update_mix()
 
-	def update_recipe_status(self):
-		if not self.is_init:
-			return
+    def update_recipe_status(self):
+        if not self.is_init:
+            return
 
-		current_recipe = str(self.ui.recipe_box.currentText())
-		total_flav = self.be.get_total_flavor(current_recipe)
-		if total_flav is None:
-			self.ui.status_bar.showMessage('Recipe %s not found!'%current_recipe)
-		else:
-			total_flav = total_flav * 100.0
-			self.ui.status_bar.showMessage('Recipe: %s; Total Flavor: %.1f%%; Max VG: %.1f%%'%(
-											current_recipe, total_flav, 100.0-total_flav))
+        current_recipe = str(self.ui.recipe_box.currentText())
+        total_flav = self.be.get_total_flavor(current_recipe)
+        if total_flav is None:
+            self.ui.status_bar.showMessage('Recipe %s not found!'%current_recipe)
+        else:
+            total_flav = total_flav * 100.0
+            self.ui.status_bar.showMessage('Recipe: %s; Total Flavor: %.1f%%; Max VG: %.1f%%'%(
+                                            current_recipe, total_flav, 100.0-total_flav))
 
-	def check_inputs(self):
-		""" Read the mix parameters from the UI elements and convert numbers to type float.
-			If float() returns a value error, mark that input box red and return None, otherwise
-			return a tuple of parameters that can be passed directly to backend.calculate_mix()
-		"""
+    def check_inputs(self):
+        """ Read the mix parameters from the UI elements and convert numbers to type float.
+            If float() returns a value error, mark that input box red and return None, otherwise
+            return a tuple of parameters that can be passed directly to backend.calculate_mix()
+        """
 
-		# We can always make these strings, and strings are expected anyway so no error checking
-		# calculate_mix will make sure that the selected recipe exists
-		recipe = str(self.ui.recipe_box.currentText())
-		mix = str(self.ui.mix_box.itemData(self.ui.mix_box.currentIndex()))
+        # We can always make these strings, and strings are expected anyway so no error checking
+        # calculate_mix will make sure that the selected recipe exists
+        recipe = str(self.ui.recipe_box.currentText())
+        mix = str(self.ui.mix_box.itemData(self.ui.mix_box.currentIndex()))
 
-		err = False
-		try:
-			totalvol = float(self.ui.totalvol_box.text())
-			if totalvol <= 0:
-				raise ValueError
-			self.ui.totalvol_box.setStyleSheet('')
-		except ValueError:
-			self.ui.totalvol_box.setStyleSheet('background-color: rgb(255, 102, 102);')
-			err = True
+        err = False
+        try:
+            totalvol = float(self.ui.totalvol_box.text())
+            if totalvol <= 0:
+                raise ValueError
+            self.ui.totalvol_box.setStyleSheet('')
+        except ValueError:
+            self.ui.totalvol_box.setStyleSheet('background-color: rgb(255, 102, 102);')
+            err = True
 
-		try:
-			nic = float(self.ui.nic_box.text())
-			if nic < 0:
-				raise ValueError
-			self.ui.nic_box.setStyleSheet('')
-		except ValueError:
-			self.ui.nic_box.setStyleSheet('background-color: rgb(255, 102, 102);')
-			err = True
+        try:
+            nic = float(self.ui.nic_box.text())
+            if nic < 0:
+                raise ValueError
+            self.ui.nic_box.setStyleSheet('')
+        except ValueError:
+            self.ui.nic_box.setStyleSheet('background-color: rgb(255, 102, 102);')
+            err = True
 
-		try:
-			vg = float(self.ui.vg_box.text())
-			if vg < 0:
-				raise ValueError
-			self.ui.vg_box.setStyleSheet('')
-		except ValueError:
-			self.ui.vg_box.setStyleSheet('background-color: rgb(255, 102, 102);')
-			err = True
+        try:
+            vg = float(self.ui.vg_box.text())
+            if vg < 0:
+                raise ValueError
+            self.ui.vg_box.setStyleSheet('')
+        except ValueError:
+            self.ui.vg_box.setStyleSheet('background-color: rgb(255, 102, 102);')
+            err = True
 
-		return None if err else (recipe, totalvol, nic, vg, mix)
+        return None if err else (recipe, totalvol, nic, vg, mix)
 
 
-	def exit(self):
-		QtCore.QCoreApplication.instance().quit()
+    def exit(self):
+        QtCore.QCoreApplication.instance().quit()
 
 
 if __name__ == "__main__":
-	app = QtGui.QApplication(sys.argv)
-	myapp = YaccMain()
-	myapp.show()
-	sys.exit(app.exec_())
+    app = QtGui.QApplication(sys.argv)
+    myapp = YaccMain()
+    myapp.show()
+    sys.exit(app.exec_())
